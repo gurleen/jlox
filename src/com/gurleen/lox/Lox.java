fd1b263f9e4ed8e3aec943b8ddf7f6f46a6a8623
Gurleen Singh <gurleen@wewager.io>
2021-09-13 19:51:04 -0400

    Completed Chapter 6

--- src/com/gurleen/lox/Lox.java
+++ src/com/gurleen/lox/Lox.java
@@ -26,10 +26,12 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for(Token token : tokens) {
-            System.out.println(token);
-        }
+        if(hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -39,6 +41,14 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
     private static void runPrompt() throws IOException {
