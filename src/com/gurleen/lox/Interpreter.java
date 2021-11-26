84db93c9f44edfbcdf63ba311438e29ffb4ad7fa
Gurleen Singh <gurleen@wewager.io>
2021-11-25 22:41:49 -0500

    Added statement syntax and print statements

--- src/com/gurleen/lox/Interpreter.java
+++ src/com/gurleen/lox/Interpreter.java
@@ -1,14 +1,21 @@
 package com.gurleen.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-    void interpret(Expr expression) {
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
+    }
+
+    private void execute(Stmt statement) {
+        statement.accept(this);
     }
 
     private String stringify(Object object) {
@@ -119,6 +126,19 @@ public class Interpreter implements Expr.Visitor<Object> {
                 return -(double)right;
         }
 
+        return null;
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
         return null;
     }
 }
